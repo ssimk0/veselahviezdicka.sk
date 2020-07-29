09ded79d6222e54e4187a9032bedb641ebe6b9c2
sebo <contact@sebastiansimko.sk>
2020-07-29 16:46:29 +0200

    add information to contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,47 @@
 <template>
-  <div id="contact">
-    <h1>This is an Contact page</h1>
+  <div id="contact" class="row mt-3">
+    <b-col>
+      <h2>Kontakt</h2>
+      <hr/>
+      <div>
+        <p class="font-weight-bold">
+          Jasle Veselá Hviezdička
+        </p>
+        <div>
+          <article>
+            Čínska 24
+          </article>
+          <article>
+            040 13 Košice
+          </article>
+        </div>
+        <div class="mt-4">
+          <p class="font-weight-bold">
+            Kontaktná osoba:
+          </p>
+          <article>Mgr. Katarína Zaujecová</article>
+          <a href="tel:+421907 339 915">0907 339 915</a> <br/>
+          <a href="mailto:veselahviezdicka@gmail.com">veselahviezdicka@gmail.com</a>
+        </div>
+        <div class="mt-4">
+          <p class="font-weight-bold">
+            Prevádzkovateľ:
+          </p>
+          <article>Občianske združenie Veselá Hviezdička</article>
+          <article>Sídlo: Školská 790,951 73 Jelenec5</article>
+          <article>ICO: </article>
+          <article>DIC: </article>
+          <article>IBAN: </article>
+        </div>
+      </div>
+    </b-col>
+    <b-col>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2630.2838477030086!2d21.27225241596874!3d48.75737551576755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473ee0b8161d0db9%3A0xda18b65fa9ffebf5!2zTWF0ZXJza8OhIMWga29sYSDEjMOtbnNrYQ!5e0!3m2!1sfr!2ssk!4v1596033093113!5m2!1sfr!2ssk"
+        width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""
+        aria-hidden="false"
+        tabindex="0"></iframe>
+    </b-col>
   </div>
 </template>
 
