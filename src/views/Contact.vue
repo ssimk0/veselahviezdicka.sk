19e2df7439a24c53ab0362cecc6bf76b80fb4584
sebo <contact@sebastiansimko.sk>
2020-08-01 07:33:09 +0200

    improve contact and top bar

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="contact" class="row mt-3">
-    <b-col>
+    <b-col cols="12" md="6" class="text-center text-md-left">
       <h2>Kontakt</h2>
       <hr/>
       <div>
@@ -35,7 +35,7 @@
         </div>
       </div>
     </b-col>
-    <b-col>
+    <b-col cols="12" md="6">
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2630.2838477030086!2d21.27225241596874!3d48.75737551576755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473ee0b8161d0db9%3A0xda18b65fa9ffebf5!2zTWF0ZXJza8OhIMWga29sYSDEjMOtbnNrYQ!5e0!3m2!1sfr!2ssk!4v1596033093113!5m2!1sfr!2ssk"
         width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""
